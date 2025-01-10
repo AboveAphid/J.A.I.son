34bb4918c78589c60bc0b7bcd8d67751dbdd6bb8
limitcantcode <limit@limit-cc.com>
2025-01-10 22:59:51 +0100

    [feature] Component-driven architecture
    - introduce components
    - added graceful shutdown to cleanup child processes
    
    Signed-off-by: limitcantcode <limit@limit-cc.com>

--- src/main.py
+++ src/main.py
@@ -4,10 +4,12 @@ load_dotenv()
 from utils.args import args
 from utils.jaison import JAIson
 from utils.discord import DiscordBot
+from utils.signal import GracefulKiller
 from webui import start_ui
 from threading import Thread
 
 jaison_main = JAIson(init_config_file=args.config)
+kill_handler = GracefulKiller(jaison_main)
 
 # For Discord Bot
 discord_bot = DiscordBot(jaison_main)
